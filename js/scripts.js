048d76b794b6477ed5a056d1909307212a04a2ad
VICTOR GABRIEL GOMES SILVA <Victor Gabriel>
2025-04-08 21:16:56 -0300

    08/04

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function calcular(){
+function calcular() {
     // serão criadas duas variáveis
     // em js as variáveis não possuem tipo
     let mascote, homenagem, leite, kit, suplemento, soma, equipe
@@ -16,20 +16,20 @@ function calcular(){
     soma = mascote + homenagem + (2 * (leite))
     // vamos calcular a pontuação considerando as metas de kit e suplemento
     equipe = document.getElementById("equipe").value
-    if (equipe == "Laranja"){
-        if (kit >= 97){
-            soma = soma + 5000 + ((kit-97) * 30)
-         }
-        else if (kit >= 78){
-            soma = soma + 4000 ((kit-78) * 30)
+    if (equipe == "Laranja") {
+        if (kit >= 97 && suplemento >= 49) {
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
         }
-        else if (kit >= 49){
-            soma = soma + 2500 ((kit-49) * 30)
+        else if ((kit >= 78 && suplemento >= 39)) {
+            soma = soma + 4000((kit - 78) * 30) + ((suplemento - 39) * 15)
         }
-        else if (kit >= 19){
-            soma = soma + 1000 ((kit-19) * 30)
+        else if (kit >= 49 && suplemento >= 25) {
+            soma = soma + 2500((kit - 49) * 30) + ((suplemento - 25) * 15)
         }
-    } 
+        else if (kit >= 19 && suplemento >= 10) {
+            soma = soma + 1000((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
+    }
     // retorna o valor ao HTML
     // template string
     document.getElementById("soma").innerHTML = `A soma é: ${soma}`
